e1521529e541af0eb26a91f83b8cf47050d633cf
Hilaire Bekono <mbhd90@gmail.com>
2022-04-23 14:05:13 +0200

    first commit

---
+++ src/main/java/com/bmtracking/motodey/MotodeyApplication.java
@@ -0,0 +1,13 @@
+package com.bmtracking.motodey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotodeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotodeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmtracking/motodey/MotodeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmtracking.motodey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotodeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
